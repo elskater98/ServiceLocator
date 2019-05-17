5d9a4e295472828648bc36996340e7b37190d080
MarKraken <mvisapascual@gmail.com>
2019-05-17 20:38:02 +0200

    Millorat CachedServiceLocator.

--- src/servicelocator/CachedServiceLocator.java
+++ src/servicelocator/CachedServiceLocator.java
@@ -4,21 +4,23 @@ import java.util.HashMap;
 
 public class CachedServiceLocator implements ServiceLocator {
     private HashMap<String, Object> cache;
-    private HashMap<String, Object> services;
+    private HashMap<String, Factory> factories;
 
     public CachedServiceLocator(){
-        services = new HashMap<>();
+        factories = new HashMap<>();
         cache = new HashMap<>();
     }
 
     public void setService(String name, Factory factory) throws LocatorError {
-        if (!services.containsKey(name))
-            services.put(name, factory);
+        if (!factories.containsKey(name))
+            factories.put(name, factory);
         else
             throw new LocatorError(new ClassCastException());
     }
 
     public void setConstant(String name, Object value) throws LocatorError {
+        /*Per l'informe*/ /*Com que es guardaran objectes tan si son constants com els objectes que crein les factories
+                            no cal un tercer hashmap per fer de cache d'objectes creats per factories*/
         if (!cache.containsKey(name))
             cache.put(name, value);
         else
@@ -28,8 +30,8 @@ public class CachedServiceLocator implements ServiceLocator {
     public Object getObject(String name) throws LocatorError {
         if (cache.containsKey(name))
             return cache.get(name);
-        else if(services.containsKey(name)) {
-            cache.put(name, ((Factory) services.get(name)).create(this));
+        else if(factories.containsKey(name)) {
+            cache.put(name, (factories.get(name)).create(this));
             return cache.get(name);
         } else throw new LocatorError(new ClassCastException());
     }
